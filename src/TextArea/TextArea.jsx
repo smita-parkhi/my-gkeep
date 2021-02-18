32f1620b9aa042f77904d5cd166f2f7e609b0ee3
Smita Parkhi <smitaparkhi@Pravins-MBP.domain.name>
2021-02-18 15:43:29 +0530

    fix outside click event

--- src/TextArea/TextArea.jsx
+++ src/TextArea/TextArea.jsx
@@ -1,40 +1,56 @@
-import react, { useState,useRef } from 'react';
+import react, { useState, useRef, useEffect } from 'react';
 
 import './textarea.scss';
 
 
-const TextArea = (props) =>{
-    const [showdata,setdata]=useState(false);
+const TextArea = (props) => {
+    const [showdata, setdata] = useState(false);
 
     const textInput = useRef(null);
+    const noteCreateBox=useRef(null);
 
 
-    const handleOnClick = () =>{
-        setdata(!showdata);
+    const handleOnClick = () => {
+        // setdata(!showdata);
+        setdata(true);
         textInput.current.focus();
 
+        
+
     }
-    return(
-        <div className='container_wrapper_text'>
-            <div className='input_field' style={{height : showdata ? '100px' : '30px'}}>
+
+    const handleClickOutside = (e) =>{
+        
+        if (!noteCreateBox.current.contains(e.target)) {
+            setdata(false);
+        }
+    }
+
+    useEffect(()=>{
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    })
+    
+    return (
+        <div className='container_wrapper_text' ref={noteCreateBox}>
+            <div className='input_field'
+            >
+                <div>
+                    {showdata ? <input
+                        className='text_input'
+                        type="text"
+                        placeholder='Title'
+
+                    /> : null}
+                </div>
                 <input
-                 className='text_input' 
-                 type="text" 
-                 placeholder='Title'
-                 onClick={handleOnClick}
-                 
-
-                 />
-                <input 
-                className='multiline_input' 
-                type='text' 
-                placeholder='Take a note'
-                ref={textInput} >
-
-                </input>
-
+                    className='multiline_input'
+                    type='text'
+                    placeholder='Take a note...'
+                    ref={textInput}
+                    onClick={handleOnClick}
+                />
             </div>
-
         </div>
 
     );
